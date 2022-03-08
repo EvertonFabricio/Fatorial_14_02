7a9195c74627c2b65dc938c5300fb04164cc257b
EvertonFabricio <everton.5by5@gmail.com>
2022-03-08 10:29:28 -0300

    Adicionar arquivos de projeto.

---
+++ Fatorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fat = 1, n = 0;
+            Console.WriteLine("Digite um número inteiro maior que zero para calcularmos seu fatorial: ");
+            int numero = int.Parse(Console.ReadLine());
+            n = numero;
+
+
+            if (n == 0)
+                Console.WriteLine("\nO fatorial do número 0 é igual a 1.");
+            else if (n > 0)
+            {
+                do
+                {
+                    fat = fat * n;
+                    n = n - 1;
+                }
+                while (n > 0);
+
+                Console.WriteLine($"\nO fatorial do número {numero} é igual a {fat}.");
+            }
+            else
+            {
+                Console.WriteLine("Tente novamente digitando um número positivo.");
+            }
+           
+        }
+    }
+}
